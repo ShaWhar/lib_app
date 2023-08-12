fbe4e86ff7585b0634bfa244bdf51a5c2091e9fa
ShaWhar <mt418605@gmail.com>
2023-08-12 22:44:26 +0630

    second commit

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -6,40 +6,48 @@ import '../bloc/home_page_bloc.dart';
 import '../bloc/search_bloc.dart';
 import '../bloc/shelf_bloc.dart';
 
+// Extension for common navigation-related operations
+extension Navigation on BuildContext {
+  // Method to retrieve an instance of EBooksBloc from the widget tree
+  EBooksBloc getEbooksYourBooksPageBlocInstance() => read<EBooksBloc>();
 
+  // Method to retrieve an instance of HomePageBloc from the widget tree
+  HomePageBloc getHomePageBlocInstance() => read<HomePageBloc>();
 
-extension Navigation on BuildContext{
-  EBooksBloc getEbooksYourBooksPageBlocInstance()=>read<EBooksBloc>();
+  // Method to retrieve an instance of ShelfBooksBloc from the widget tree
+  ShelfBooksBloc getShelvesTabBlocInstance() => read<ShelfBooksBloc>();
 
-  HomePageBloc getHomePageBlocInstance()=>read<HomePageBloc>();
+  // Method to retrieve an instance of SearchPageBloc from the widget tree
+  SearchPageBloc getSearchPageBlocInstance() => read<SearchPageBloc>();
 
-  ShelfBooksBloc getShelvesTabBlocInstance()=>read<ShelfBooksBloc>();
+  // Method to navigate back to the previous screen
+  void navigateBack(BuildContext context) => Navigator.of(context).pop();
 
-  SearchPageBloc getSearchPageBlocInstance()=>read<SearchPageBloc>();
-
-  void navigateBack(BuildContext context)=>Navigator.of(context).pop();
-
-  Future navigateToNextScreen(BuildContext context,Widget nextScreen)=>
+  // Method to navigate to the next screen and push it onto the navigation stack
+  Future navigateToNextScreen(BuildContext context, Widget nextScreen) =>
       Navigator.of(context).push(MaterialPageRoute(builder: (context) => nextScreen));
 
-  Future navigateToNextScreenReplace(BuildContext context,Widget nextScreen)=>
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => nextScreen,));
+  // Method to navigate to the next screen and replace the current screen
+  Future navigateToNextScreenReplace(BuildContext context, Widget nextScreen) =>
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => nextScreen));
 }
 
-extension TextCorrection on String{
-  String checkCount(){
-    if(this=='0'){
+// Extension for common text manipulation and correction operations
+extension TextCorrection on String {
+  // Method to check the count and return a modified string based on the value
+  String checkCount() {
+    if (this == '0') {
       return 'Empty';
-    }else if(this=='1'){
+    } else if (this == '1') {
       return '$this book';
-    }
-    else{
+    } else {
       return '$this books';
     }
   }
 
-  String  nullDefaultImage(){
-    if(isEmpty){
+  // Method to provide a default image URL if the string is empty
+  String nullDefaultImage() {
+    if (isEmpty) {
       return 'https://climate.onep.go.th/wp-content/uploads/2020/01/default-image.jpg';
     }
     return this;
